e08ee47d2035a080c7a877a0c43339a6daa4c679
alangomes7 <alan.gomes7email@gmail.com>
2023-12-23 15:07:19 -0300

    some code corrections to run

--- source_files/py/server.py
+++ source_files/py/server.py
@@ -1,13 +1,10 @@
 import threading
-import socket
 import time
-from datetime import datetime
-import os
 
-import message as message_manager
+from server_utils import *
+from ui_files import ui_server_glade
 
 import gi
-from ui_files import ui_server_glade
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
@@ -27,7 +24,7 @@ class ServerInterface(Gtk.Window):
 
         # Connect signals to callback functions
         self.button_run_stop.connect("clicked", self.on_button_run_stop_clicked)
-        self.window.connect("destroy", self.on_main_window_destroy)
+        self.window.connect("delete-event", self.on_main_window_destroy)
 
         # Define constants and variables
         self.BUFFER_SIZE = 1024
@@ -48,62 +45,86 @@ class ServerInterface(Gtk.Window):
 
     def run(self):
         """Runs the server."""
-        self.window.show_all()
+        GLib.idle_add(self.window.show_all)
         Gtk.main()
 
-    def on_main_window_destroy(self, window):
+    def on_main_window_destroy(self, window, event=None):
         """Closes the interface and the server."""
-        self.close_server()
-        Gtk.main_quit()
+        thread_close_server = threading.Thread(target=self.close_server, args=(True,))
+        thread_close_server.start()
+        return True
 
     def on_button_run_stop_clicked(self, button):
         """Handles the click event of the 'Run/Stop' button."""
         if not self.server_running:
             self.socket_server = socket_connect(self.server_running, self.LOG_FILE_NAME, self.text_buffer)
             if not self.socket_server:
-                self.log_file("Socket not connected!\n")
+                log_file("Socket not connected!\n", self.LOG_FILE_NAME, self.text_buffer)
             else:
                 self.server_running = True
-                run_server = threading.Thread(target=self.control_server, args=())
+                run_server = threading.Thread(target=self.control_server)
                 run_server.start()
                 self.button_run_stop.set_label("Close server")
         else:
-            time.sleep(4)
-            self.button_run_stop.set_label("Start server")
-            self.server_running = False
+            thread_close_server = threading.Thread(target=self.close_server())
+            thread_close_server.start()
 
     # Server Control Functions
 
+    def button_run_stop_stopping_server(self):
+        """Wait the server safely stop to update the start button."""
+        self.button_run_stop.set_label("Start server")
+        self.button_run_stop.set_sensitive(False)
+        # Sleep is necessary to server safe stop.
+        time.sleep(3)
+        self.button_run_stop.set_sensitive(True)
+
     def control_server(self):
         """Controls the server's main loop."""
-        self.thread_server_listening = threading.Thread(target=self.server_listen, args=(), daemon=True)
-        self.thread_server_listening.start()
+        if self.server_running:
+            self.thread_server_listening = threading.Thread(target=self.server_listen, daemon=True)
+            self.thread_server_listening.start()
+        i = 0
         while self.server_running:
             pass
-        self.close_server()
 
     def server_checks(self, socket_error=""):
         """
         Verify if the server was closed due to a client message or error.
         :param socket_error: states error message. Default is "".
         """
+        time.sleep(3)
         if self.server_running:
-            log_file(socket_error)
+            log_file(socket_error, self.LOG_FILE_NAME, self.text_buffer)
             log_file("Restarting the server...\n"
                      "Server restarted.\n "
-                     "--------------\n")
+                     "--------------\n",
+                     self.LOG_FILE_NAME, self.text_buffer)
             self.control_server()
 
-    def close_server(self):
-        time.sleep(2)
-        self.socket_server.shutdown(socket.SHUT_RDWR)
-        self.socket_server.close()
-        log_file("Server stopped.\n", self.LOG_FILE_NAME, self.text_buffer)
+    def close_server(self, close_interface=False):
+        """Close the server and/or the program.
+        :param close_interface: states if the interface must be closed. Default is False.
+        """
+        if self.server_running:
+            self.server_running = False
+            self.socket_server.shutdown(socket.SHUT_RDWR)
+            self.socket_server.close()
+            log_file("Server stopped.\n", self.LOG_FILE_NAME, self.text_buffer)
+            button_run_stop_thread = threading.Thread(target=self.button_run_stop_stopping_server)
+            button_run_stop_thread.start()
+        if close_interface:
+            log_file("Exiting...\n"
+                     "--------------------------------"
+                     , self.LOG_FILE_NAME, self.text_buffer)
+            time.sleep(2)
+            self.window.destroy()
+            Gtk.main_quit()
 
     def server_listen(self):
         """Listens for incoming client connections and processes messages."""
-        while self.server_running:
-            try:
+        try:
+            while self.server_running:
                 socket_client, address = self.socket_server.accept()
                 self.server_sends(message_manager.protocol_message_encoding("server", "client", "name"), socket_client)
 
@@ -134,10 +155,8 @@ class ServerInterface(Gtk.Window):
                         "server", received_message[3], message_manager.OPCODE_CONNECTION_ERROR, message_data)
                     self.log_file("Sent: %s" % protocol_message)
                     self.server_sends(protocol_message, socket_client)
-            except socket.error as socket_error:
-                self.server_checks(str(socket_error))
-                break
-        self.server_checks()
+        except Exception as error:
+            self.server_checks(str(error))
 
     def handle_client(self, client):
         """Processes the messages received from a specific client.
@@ -154,18 +173,63 @@ class ServerInterface(Gtk.Window):
                 self.user_sender = message_received[0]
                 self.message_confirmation(self.user_sender, message_received[2])
 
+                # UNKNOWN_OPERATION
                 if message_received[2] == message_manager.OPCODE_UNKNOWN_OPERATION:
                     message_data = "Operation not supported!"
                     protocol_message = message_manager.protocol_message_encoding(
                         "server", self.user_sender, message_manager.OPCODE_UNKNOWN_OPERATION, message_data)
                     self.server_sends(protocol_message)
                     continue
+                # OPCODE_ECHO
                 if message_received[2] == message_manager.OPCODE_ECHO:
                     protocol_message = message_manager.protocol_message_encoding(
                         self.user_sender, self.user_sender, message_manager.OPCODE_ECHO, message_received[3])
                     self.server_sends(protocol_message)
                     continue
-                # Add more conditions for handling different message types
+                # PRIVATE_MESSAGE
+                if message_received[2] == message_manager.OPCODE_PRIVATE_MESSAGE:
+                    protocol_message = message_manager.protocol_message_encoding(
+                        self.user_sender, message_received[1], message_manager.OPCODE_PRIVATE_MESSAGE,
+                        message_received[3])
+                    self.server_sends(protocol_message)
+                    continue
+                # BROADCAST
+                if message_received[2] == message_manager.OPCODE_BROADCAST \
+                        or message_received[2] == message_manager.OPCODE_BROADCAST_NOT_ME:
+                    protocol_message = message_manager.protocol_message_encoding(
+                        self.user_sender, message_manager.OPCODE_BROADCAST, message_received[2], message_received[3])
+                    self.server_sends(protocol_message)
+                    continue
+                # LIST_CLIENTS
+                if message_received[2] == message_manager.OPCODE_LIST_CLIENTS and message_received[1] == "server":
+                    protocol_message = message_manager.protocol_message_encoding(
+                        self.SERVER_ITSELF, self.user_sender, message_manager.OPCODE_LIST_CLIENTS, self.get_all_list())
+                    self.server_sends(protocol_message)
+                    continue
+                # EXIT_CLIENT
+                if message_received[2] == message_manager.OPCODE_EXIT_CLIENT:
+                    # Message sends to client before the client left
+                    message_data = "You are exiting the server"
+                    protocol_message = message_manager.protocol_message_encoding(
+                        self.SERVER_ITSELF, self.user_sender, message_manager.OPCODE_EXIT_CLIENT, message_data)
+                    self.server_sends(protocol_message)
+                    # Tell everyone that clients left
+                    message_data = "client (" + self.user_sender + ") left"
+                    protocol_message = message_manager.protocol_message_encoding(
+                        self.SERVER_ITSELF, message_manager.OPCODE_BROADCAST, message_manager.OPCODE_EXIT_CLIENT,
+                        message_data)
+                    self.server_sends(protocol_message)
+                    # Finally removes it from connected clients
+                    self.remove_client(self.user_sender)
+                    continue
+                # VIDEO_CONFERENCE, just forward
+                elif message_received[2] in (
+                        message_manager.OPCODE_VIDEO_CONFERENCE, message_manager.OPCODE_CLIENT_ADDRESS_REQUEST,
+                        message_manager.OPCODE_CLIENT_ADDRESS_SEND, message_manager.OPCODE_CLIENT_AVAILABLE):
+                    protocol_message = message_manager.protocol_message_encoding(
+                        message_received[0], message_received[1], message_received[2], message_received[3])
+                    self.server_sends(protocol_message)
+                    continue
 
             except socket.error as socket_error:
                 message_data = str(socket_error) + " \n; "
@@ -178,165 +242,7 @@ class ServerInterface(Gtk.Window):
                     self.server_sends(protocol_message)
                     self.remove_client(message_received[0])
                 break
-        if self.server_running:
-            self.log_file("Closing server...")
-            message_data += "\n User (" + message_received[0] + ") closes the server."
-            protocol_message = message_manager.protocol_message_encoding(
-                self.SERVER_ITSELF, self.SENDS_TO_ALL, message_manager.OPCODE_BROADCAST, message_data)
-            self.server_sends(protocol_message)
         self.server_checks()
-
-
-# Helper Functions
-
-def create_folder(folder_path):
-    """Creates a folder to save all log messages.
-    :param folder_path: path to create a folder."""
-    try:
-        if os.path.exists(folder_path):
-            pass
-        else:
-            os.makedirs(folder_path)
-    except OSError as e:
-        print("Error creating log folder")
-
-
-def get_time(spaces=True):
-    """Gets the current time to log messages. :param spaces: default is True. That means the log will include spaces
-    to separate date and time. Otherwise, is used (_) underscore."""
-    current_time = datetime.now()
-    formatted_time = ""
-    if spaces:
-        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    else:
-        formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")
-    return formatted_time
-
-
-def get_client_socket_by_name(client_name, clients_connected):
-    """Function to search for a client by name.
-    :param client_name: client name to scour.
-    :param clients_connected: list of connected clients."""
-    client_socket = clients_connected.get(client_name, "not found")
-    return client_socket
-
-
-def dict_empty(dict_clients):
-    """Verify if dict is empty.
-    :param dict_clients: dictionary with all clients connected."""
-    return not bool(dict_clients)
-
-
-def save_log_file(log_message, log_file_name):
-    """Saves log messages into a file.
-    :param log_message: log message to be saved on log file.
-    :param log_file_name: log file name to save messages."""
-    try:
-        with open(log_file_name, 'a') as file:
-            file.write(str(log_message) + '\n')
-    except OSError as e:
-        print(e)
-
-
-def update_log_interface(log_message, text_buffer):
-    """Prints log messages on the interface.
-    :param log_message: log message to be printed on screen.
-    :param text_buffer: text buffer by interface."""
-    end_iter = text_buffer.get_end_iter()
-    text_buffer.insert(end_iter, log_message + "\n")
-
-
-def log_file(log_message, log_file_name, text_buffer):
-    """Saves log file and prints on the interface.
-    :param log_message: log message to be printed on screen and saved on log file.
-    :param log_file_name: log file name to save messages.
-    :param text_buffer: text buffer by interface."""
-    GLib.idle_add(save_log_file, log_message, log_file_name)
-    GLib.idle_add(update_log_interface, log_message, text_buffer)
-
-
-def message_confirmation(user_sender, message_code, server_name):
-    """Sends the message confirmation to a client.
-    :param user_sender: user that sends the message.
-    :param message_code: message code."""
-    if message_code in (message_manager.OPCODE_PRIVATE_MESSAGE, message_manager.OPCODE_BROADCAST_NOT_ME):
-        server_sends(message_manager.protocol_message_encoding(
-            server_name, user_sender, message_manager.OPCODE_MESSAGE_CONFIRMATION, "message sent"))
-
-
-def server_sends(protocol_message, clients_connected, socket_client=None):
-    """Prints the server message and sends it.
-    :param protocol_message: message to send.
-    :param clients_connected: list of connected clients.
-    :param socket_client: client that sends the message. Default is None."""
-    log_file("Server sends: %s" % protocol_message)
-    if not socket_client:
-        if clients_connected:
-            message_manager.send_server_message(protocol_message, clients_connected)
-        else:
-            log_file("Server clients is empty!")
-    else:
-        socket_client.send(protocol_message)
-
-
-def get_all_list(clients_connected):
-    """Function to show the list of connected clients in all pairs (clients, address).
-    :param clients_connected: list of connected clients."""
-    all_items = ""
-    for key, value in clients_connected.items():
-        address = str(value).split("raddr=")[1][:-1]
-        all_items += key + " -> " + address + "\n"
-    return all_items
-
-
-def add_client(username, clients_connected, client_socket):
-    """Add client to the dictionary.
-    :param username: username by client socket.
-    :param clients_connected: list of connected clients.
-    :param client_socket: socket client to add."""
-    clients_connected[username] = client_socket
-
-
-def remove_client(client_username, clients_connected):
-    """Removes a client from the dictionary.
-    :param clients_connected: list of connected clients.
-    :param client_username: username by client."""
-    clients_connected.pop(client_username, -1)
-
-
-def get_local_ip():
-    """Function to get the local IP address."""
-    try:
-        hostname = socket.gethostname()
-        local_ip = socket.gethostbyname(hostname)
-        return local_ip
-
-    except socket.error as e:
-        log_file(f"Error: {e}")
-        return None
-
-
-def socket_connect(server_running, log_file_name, text_buffer):
-    """Function to create a socket and connect it.
-    :param server_running: flag to indicate if the server is running or not.
-    :param log_file_name: log file name to save messages.
-    :param text_buffer: text buffer by interface."""
-    socket_connecting = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    host = get_local_ip()
-    port = 5500
-    try:
-        if not server_running:
-            log_file("Connection attempt...", log_file_name, text_buffer)
-            socket_connecting.bind((host, port))
-            socket_connecting.listen(10)
-            log_file("Server is running and listening on (%s,%s)\n" % (host, port), log_file_name, text_buffer)
-            return socket_connecting
-        else:
-            return None
-    except socket.error as e:
-        log_file(f"Error: {e}", log_file_name, text_buffer)
-        log_file("Server connection error on (%s,%s)" % (host, port), log_file_name, text_buffer)
-        return None
 
 
 # Main Function
---
+++ source_files/py/server_utils.py
@@ -0,0 +1,160 @@
+# Helper Functions
+import socket
+from datetime import datetime
+import os
+import message as message_manager
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GLib
+
+
+def create_folder(folder_path):
+    """Creates a folder to save all log messages.
+    :param folder_path: path to create a folder."""
+    try:
+        if os.path.exists(folder_path):
+            pass
+        else:
+            os.makedirs(folder_path)
+    except OSError as e:
+        print("Error creating log folder")
+
+
+def get_time(spaces=True):
+    """Gets the current time to log messages. :param spaces: default is True. That means the log will include spaces
+    to separate date and time. Otherwise, is used (_) underscore."""
+    current_time = datetime.now()
+    formatted_time = ""
+    if spaces:
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+    return formatted_time
+
+
+def get_client_socket_by_name(client_name, clients_connected):
+    """Function to search for a client by name.
+    :param client_name: client name to scour.
+    :param clients_connected: list of connected clients."""
+    client_socket = clients_connected.get(client_name, "not found")
+    return client_socket
+
+
+def dict_empty(dict_clients):
+    """Verify if dict is empty.
+    :param dict_clients: dictionary with all clients connected."""
+    return not bool(dict_clients)
+
+
+def save_log_file(log_message, log_file_name):
+    """Saves log messages into a file.
+    :param log_message: log message to be saved on log file.
+    :param log_file_name: log file name to save messages."""
+    try:
+        with open(log_file_name, 'a') as file:
+            file.write(str(log_message) + '\n')
+    except OSError as e:
+        print(e)
+
+
+def update_log_interface(log_message, text_buffer):
+    """Prints log messages on the interface.
+    :param log_message: log message to be printed on screen.
+    :param text_buffer: text buffer by interface."""
+    end_iter = text_buffer.get_end_iter()
+    text_buffer.insert(end_iter, log_message + "\n")
+
+
+def log_file(log_message, log_file_name, text_buffer):
+    """Saves log file and prints on the interface.
+    :param log_message: log message to be printed on screen and saved on log file.
+    :param log_file_name: log file name to save messages.
+    :param text_buffer: text buffer by interface."""
+    GLib.idle_add(save_log_file, log_message, log_file_name)
+    GLib.idle_add(update_log_interface, log_message, text_buffer)
+
+
+def message_confirmation(user_sender, message_code, server_name):
+    """Sends the message confirmation to a client.
+    :param user_sender: user that sends the message.
+    :param message_code: message code."""
+    if message_code in (message_manager.OPCODE_PRIVATE_MESSAGE, message_manager.OPCODE_BROADCAST_NOT_ME):
+        server_sends(message_manager.protocol_message_encoding(
+            server_name, user_sender, message_manager.OPCODE_MESSAGE_CONFIRMATION, "message sent"))
+
+
+def server_sends(protocol_message, clients_connected, socket_client=None):
+    """Prints the server message and sends it.
+    :param protocol_message: message to send.
+    :param clients_connected: list of connected clients.
+    :param socket_client: client that sends the message. Default is None."""
+    log_file("Server sends: %s" % protocol_message)
+    if not socket_client:
+        if clients_connected:
+            message_manager.send_server_message(protocol_message, clients_connected)
+        else:
+            log_file("Server clients is empty!")
+    else:
+        socket_client.send(protocol_message)
+
+
+def get_all_list(clients_connected):
+    """Function to show the list of connected clients in all pairs (clients, address).
+    :param clients_connected: list of connected clients."""
+    all_items = ""
+    for key, value in clients_connected.items():
+        address = str(value).split("raddr=")[1][:-1]
+        all_items += key + " -> " + address + "\n"
+    return all_items
+
+
+def add_client(username, clients_connected, client_socket):
+    """Add client to the dictionary.
+    :param username: username by client socket.
+    :param clients_connected: list of connected clients.
+    :param client_socket: socket client to add."""
+    clients_connected[username] = client_socket
+
+
+def remove_client(client_username, clients_connected):
+    """Removes a client from the dictionary.
+    :param clients_connected: list of connected clients.
+    :param client_username: username by client."""
+    clients_connected.pop(client_username, -1)
+
+
+def get_local_ip():
+    """Function to get the local IP address."""
+    try:
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        return local_ip
+
+    except socket.error as e:
+        log_file(f"Error: {e}")
+        return None
+
+
+def socket_connect(server_running, log_file_name, text_buffer):
+    """Function to create a socket and connect it.
+    :param server_running: flag to indicate if the server is running or not.
+    :param log_file_name: log file name to save messages.
+    :param text_buffer: text buffer by interface."""
+    socket_connecting = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    host = get_local_ip()
+    port = 5500
+    try:
+        if not server_running:
+            log_file("Connection attempt...", log_file_name, text_buffer)
+            socket_connecting.bind((host, port))
+            socket_connecting.listen(10)
+            log_file("Server is running and listening on (%s,%s)\n" % (host, port), log_file_name, text_buffer)
+            return socket_connecting
+        else:
+            return None
+    except socket.error as e:
+        log_file(f"Error: {e}", log_file_name, text_buffer)
+        log_file("Server connection error on (%s,%s)" % (host, port), log_file_name, text_buffer)
+        return None
